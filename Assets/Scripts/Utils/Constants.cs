6a094cdc5635166d00008ba3017fcc36aec9e3ce
Cybertronnnnn <cyberg373@gmail.com>
2026-02-10 14:40:32 +0100

    Creating Constants.cs file with game constants and configuration values

---
+++ Assets/Scripts/Utils/Constants.cs
@@ -0,0 +1,17 @@
+// Constants.cs
+
+using System;
+
+namespace CyberGame.Utils {
+    public static class Constants {
+        public const string GameName = "Cyber Game";
+        public const string Version = "1.0.0";
+        public const int MaxPlayers = 4;
+        public const float Gravity = 9.81f;
+        public const string DefaultLanguage = "en";
+        public const double GameStartTime = 10.0; // in seconds
+        public static readonly DateTime GameReleaseDate = new DateTime(2026, 2, 10);
+
+        // Add more constants as needed
+    }
+}
